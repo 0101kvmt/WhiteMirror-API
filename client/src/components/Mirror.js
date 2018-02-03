8cc3b87b3ce12cb2a51cce11e86894899e5f0af7
0101kvmt <0101kvmt@gmail.com>
2018-02-03 13:39:40 -0800

    mirror style

---
+++ client/src/components/Mirror.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const MirrorWrapper = styled.div`
+  color: white;
+  text-align: right;
+  margin: 0px 10px 0px 0px;
+  width: 500px;
+  float: right;
+`;
+
+export const MirrorCrypto = styled.p``;
+
+export const MirrorTime = styled.p`
+  color: white;
+  font-size: 75px;
+  margin: 0;
+`;
+
+export const MirrorTimeAMPM = styled.p`
+  display: inline-block;
+  color: white;
+  font-size: 30px;
+  margin: 0;
+`;
+
+export const MirrorDate = styled.p`
+  color: white;
+  font-size: 30px;
+  margin: 0;
+`;
+
+export const MirrorWeather = styled.p`
+  color: white;
+  font-size: 30px;
+`;
+
+export const MirrorToDo = styled.p`
+  color: white;
+  font-size: 30px;
+`;
--- client/src/pages/mirror/index.js
+++ client/src/pages/mirror/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import styled from 'styled-components';
 
-const Title = styled.h1`
-  color: blue;
-  font-size: 100px;
-`
+import { MirrorWrapper, MirrorTime, MirrorDate, MirrorTimeAMPM, MirrorToDo } from './../../components/Mirror';
+
+
 class Mirror extends Component {
   render() {
     return (
-      <div>
-        <Title>MIRROR</Title>
+      <MirrorWrapper>
 
-      </div>
+        <MirrorDate>Today, January 28, 2018</MirrorDate>
+
+        <MirrorTime>08:54:03
+          <MirrorTimeAMPM>am</MirrorTimeAMPM>
+        </MirrorTime>
+
+        <MirrorToDo> 3:00 PM - You have a homework assignment due to Mrs. Johnsons Class </MirrorToDo>
+        <MirrorToDo> 4:54 PM - Do not forget to pick up the Avocados!</MirrorToDo>
+        <MirrorToDo> 6:00 PM - Dante invited you to make kale smoothie & tacos  </MirrorToDo>
+        <MirrorToDo> 8:50 PM - Fold laundry and organize papers.  </MirrorToDo>
+
+      </MirrorWrapper>
     );
   }
 }
